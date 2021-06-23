f2d76cc769845cb8ce731f7271817990d5264c07
Martin <martin.benes66@gmail.com>
2021-06-22 21:31:52 -0400

    added sticky nav and scroll to features

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+  /* For sticky nav */
   $('.js--section-features').waypoint(function (direction) {
 
     if (direction == "down") {
@@ -14,18 +15,43 @@ $(document).ready(function () {
     offset: '60px;'
   });
 
-  var waypoints = $('#handler-first').waypoint(function (direction) {
-    notify(this.element.id + ' hit 25% from top of window')
-  }, {
-    offset: '25%'
-  })
+
+  /*scolling to button  */
+
+  $('.js--scroll-to-plans').click(function () {
+
+    $('html, body').animate({
+      scrollTop: $('.js--section-plans').offset().top
+    }, 1000);
+
+  });
+
+  $('.js--scroll-to-start').click(function () {
+
+    $('html, body').animate({
+      scrollTop: $('.js--section-features').offset().top
+    }, 1000);
+
+  });
 
 
+  /* Navigation Scroll */
 
+  $(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+;
+  
 
-
-
-
-
-
-});
+}); //main close brackets
