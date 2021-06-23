c0b51b1bbfb403aa987490ddaad68b5b48bd1ca3
Martin <martin.benes66@gmail.com>
2021-06-22 20:59:48 -0400

    added more js code

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,31 @@
-$(document).ready(function(){
- 
+$(document).ready(function () {
+
+  $('.js--section-features').waypoint(function (direction) {
+
+    if (direction == "down") {
+      $('nav').addClass('sticky-nav');
+
+    } else {
+      $('nav').removeClass('sticky-nav');
+
+
+    }
+  }, {
+    offset: '60px;'
+  });
+
+  var waypoints = $('#handler-first').waypoint(function (direction) {
+    notify(this.element.id + ' hit 25% from top of window')
+  }, {
+    offset: '25%'
+  })
+
+
+
+
+
+
+
+
+
 });
-
-
